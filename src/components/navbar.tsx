931bb879adb3f21b5a2e5e642df6d2e01eadb20c
shotoyaar <anirudhsridhar1064@gmail.com>
2024-06-20 18:20:40 +0530

    icon cloud and 1st hero section added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,18 +1,94 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
+'use client';
+import { Fragment } from 'react'
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
-export default function Navbar() {
+const navigation = [
+  { name: 'About', href: '#', current: true },
+  { name: 'Products', href: '#', current: false },
+  { name: 'Blog', href: '#', current: false },
+  { name: 'Contests', href: '#', current: false },
+  { name: 'Contact Us', href: '#', current: false }
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Example() {
   return (
-    <div className="flex flex-row">
-      <Image
-        src="/gitlab-logo.png"
-        width={100}
-        height={100}
-        alt="gemini"
-        className="top-2 left-2"
-      />
-      <p className="text-white ml-2">hi</p>
-    </div>
-  );
+    <Disclosure as="nav" className="bg-transparent">
+      {({ open }) => (
+        <>
+          <div className="max-w-screen px-2 sm:px-6 lg:px-8">
+            <div className="relative flex h-16 items-center justify-between">
+              <div className="absolute inset-y-0 right-0flex sm:hidden">
+                {/* Mobile menu button*/}
+                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="absolute -inset-0.5" />
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </DisclosureButton>
+              </div>
+              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                    alt="Your Company"
+                  />
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4 right-0">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:text-white ease-in-out duration-300',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <DisclosurePanel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+              {navigation.map((item) => (
+                <DisclosureButton
+                  key={item.name}
+                  as="a"
+                  href={item.href}
+                  className={classNames(
+                    item.current ? 'text-gray' : 'text-gray hover:text-white ease-in-out dration-300',
+                    'block rounded-md px-3 py-2 text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </DisclosureButton>
+              ))}
+            </div>
+          </DisclosurePanel>
+        </>
+      )}
+    </Disclosure>
+  )
 }
